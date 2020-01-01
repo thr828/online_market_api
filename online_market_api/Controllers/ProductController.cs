acf2f1ddceab97c9fdd6a669966bea03fadb155b
thr828 <592040808@qq.com>
2020-01-01 22:00:41 +0800

    api

---
+++ online_market_api/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace online_market_api.Controllers
+{
+    [EnableCors("any")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            List<Product> list = new List<Product>();
+            Product p = new Product()
+            {
+                id = 1,
+                productImageUrl = "/static/productImg/product1.jpg",
+                price = 5899.99M,
+                productName= "笔记本电脑"
+            };
+            list.Add(p);
+            Product p1 = new Product()
+            {
+                id = 1,
+                productImageUrl = "/static/productImg/product2.jpg",
+                price = 5899.99M,
+                productName = "笔记本电脑2"
+            };
+            list.Add(p1);
+            Product p2 = new Product()
+            {
+                id = 1,
+                productImageUrl = "/static/productImg/product3.jpg",
+                price = 5899.99M,
+                productName = "笔记本电脑3"
+            };
+            list.Add(p2);
+            return list.ToArray();
+        }
+    }
+}
---
+++ online_market_api/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace online_market_api
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string productImageUrl { get; set; }
+
+        public decimal price { get; set; }
+
+        public string productName { get; set; }
+    }
+}
